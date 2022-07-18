bad7e7c7d9d896e8727ea13319d0600ef22c618d
Eduard Kudlanov <eduardokud@mail.ru>
2022-07-19 02:18:14 +0300

    remote commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,19 +1,19 @@
 public class CreditPaymentService {
-    public double calculate(double k, double p, double m){
+    public double calculate(double kredit, double procent, double god) {
         double result;
         double r;
         //double k = 1_000_000;//сумма
         //double p = 9.99;//ставка
-       // double m = 2;//срок в годах
-        double pp = p / 100 / 12;//ежемесечная проц ставка
-        double mm = m *12;//срок в месяцах
+        // double m = 2;//срок в годах
+        double pp = procent / 100 / 12;//ежемесечная проц ставка
+        double mm = god * 12;//срок в месяцах
         double step = 1 + pp;//1+i
         double st = Math.pow(step, mm);// возведение в степень
         double s = st - 1;// степень -1
-        double l =  pp / s;
+        double l = pp / s;
         double i = pp + l;
 
-        return result = k * i ;
+        return result = kredit * i;
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         System.out.println("Ежемесечный платеж = " + service.calculate(1_000_000, 9.99, 1));
         System.out.println();
         System.out.println("Ежемесечный платеж = " + service.calculate(1_000_000, 9.99, 2));
         System.out.println();
-        System.out.println("Ежемесечный платеж = " + service.calculate(1_000_000, 9.99, 3));
-
+        System.out.println("Ежемесечный платеж = " + service.calculate(1_000_000, 9.99, 4));
 
 
     }
